f676c78246e95929a28fb3988b0a6584e2d244f5
an0nymID <m.ihsan2131@gmail.com>
2024-04-18 19:52:19 +0800

    update server setup

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ const router = require('./routes/router');
 
 const app = express();
 
+// middleware start
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
+// middleware end
 
+// router start
 app.use('/', router);
+// router end
 
-app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
+// server port start
+const PORT = process.env.SERVER_PORT;
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT: ${PORT}`);
+});
+// server port end
