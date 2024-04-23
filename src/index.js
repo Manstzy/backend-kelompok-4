b6f9cdd93af4cb8d1ecec2110abc3153ce8c0fa6
Manstzy <muh.arhief20@gmail.com>
2024-04-23 16:15:34 +0700

    migration and configuration sequelieze

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { sequelize } = require('./models');
 
 const router = require('./routes/router');
+const { condition } = require('sequelize');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+sequelize.authenticate().then((error) => {
+    console.log("database connection has been established successfully")
+}).catch((error) => {
+    console.log("connection error" , error)
+})
+
 app.use(cors({ origin: true, credentials: true }));
 
 app.use('/', router);
 
-app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('Server Running');
+});
